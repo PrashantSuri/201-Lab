2cfe99cc1b6000e0d1d2ed45f71df6005d41d603
Prashant Suri <suripr@amazon.com>
2019-03-02 17:04:35 +0530

    Modified JS to have different answers & not just Y for all questions

--- js/script_guessing_game.js
+++ js/script_guessing_game.js
@@ -1,16 +1,25 @@
 "use strict";
-var questions = ["Do I live in Hyderabad?", "Do I work for Amazon?", "Do I like Seattle?", "Do I like Java language?", "Do I like Pizza?"];
+var questions = ["Do I live in Hyderabad?", "Do I work for Amazon?", "Do I like Superhero Movies?", "Do I know French language?", "Do I like Pizza?"];
+var answers = ["y", "y", "n", "n", "y"];
 var correct = 0;
 var i = 0;
+alert(answers[2]);
 alert("Welcome! Let's play a guessing game so that you can know me better. All Questions have to be answered in Y/N");
 for (i=0;i<questions.length;i++) {
-    var live = prompt(questions[i]);
-    console.log(questions[i] + " = " + live);
-    if (live === null) {
-        alert("Thanks for visiting");
-        break;
+    var answer = prompt(questions[i]);
+    console.log(questions[i] + " = " + answer);
+    if (answer === null) {
+        var retry = prompt("It seems you clicked cancel or typed other than Y/N. Type 'Y' if you want to try again");
+        if (retry === null || retry.trim().toLowerCase() !== "y") {
+            alert("Thanks for visiting");
+            break;
+        } 
+        else {
+            i--;
+            continue;
+        }
     }
-    else if (live.toLowerCase() === 'y') {
+    else if (answer.toLowerCase() === answers[i]) {
         alert("Correct");
         correct++;
     }
@@ -18,4 +27,4 @@ for (i=0;i<questions.length;i++) {
         alert("Sorry, incorrect answer");
     }
 }
-alert("You guessed " + correct + " answers correctly");
+alert("You guessed " + correct + " out of " + questions.length + " answers correctly");
